d5c8c6303ea89a41af69ed4a3d5b38a798b2eafe
Mike Saetern <112051948+mike-saetern@users.noreply.github.com>
2023-01-31 12:09:18 -0800

    Update algo_feb.py

--- algo_class/algo_feb.py
+++ algo_class/algo_feb.py
@@ -1,10 +1,6 @@
 #create a function that, given an input string, returns a boolean whether parenteses in the string are valid. 
 #input 'y(3(p)p(3)r)s', returns true, given 'n(0(p)3' returns False given 'n)0(t()k' returns false)
 
-# Create a function clockHandAngles(seconds) that given the number of seconds since 12:00:00, will print the angles in degress of the hour, minute and second hands.
-# there are 360 degress in a full arc rotation. Treat "straight up" 12:00:00 as 0 degress for all hands
-
-
 def check_parethesis(str):
     count = 0
     for i in str:
