793c5c5be53c34f6b8b3f969cde268a0295d7855
Mike Saetern <112051948+mike-saetern@users.noreply.github.com>
2023-01-31 12:14:19 -0800

    Update and rename basic13_algo.py to basic_algo.py

---
+++ basic_algo.py
@@ -0,0 +1,27 @@
+#Print all the integers from 1 to 255.
+ for i in range(1,256):
+     print(i)
+
+#Print integers from 0 to 255, and with each integer print the sum so far.
+ total = sum(range(1,255))
+   print(total)
+
+#Find and Print Max
+#Given an array, find and print its largest element.
+ print(max(1,5,8,99))
+
+#Array with Odds
+#Create an array with all the odd integers between 1 and 255 (inclusive).
+ arr = []
+ for i in range(1,255,2):
+     arr.append(i)
+ print(arr)
+
+# Print Odds 1-255
+# Print all odd integers from 1 to 255.
+ for i in range(1,256,2):
+     print(i)
+
+
+
+
